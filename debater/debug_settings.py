26cec320374c128ced690d6c6dd42af07760177f
Matthew Gerring <gerring.matthew@gmail.com>
2013-03-30 17:04:59 -0700

    ignored productions settings file

--- debater/debug_settings.py
+++ debater/debug_settings.py
@@ -11,6 +11,6 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 
 LIVEJS_URL = STATIC_URL+'/js/vendor/live.js' # url to live.js file
