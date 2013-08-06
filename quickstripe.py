5d6c51b65a97b91fc9691800fc8f55f5d0816070
Matthew Gerring <gerring.matthew@gmail.com>
2013-08-05 18:21:06 -0700

    removed project template files

--- quickstripe.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-ROOT = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__, static_folder=os.path.join(ROOT, 'public'), static_url_path='/public')
-app.config.from_object('config')
-app.debug = True
-
-if __name__ == '__main__':
-	app.run()
